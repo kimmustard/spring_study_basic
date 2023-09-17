b7120b03ca30731e83061867459910a9bd02ad1b
kimmustard <kimustard44@gmail.com>
2023-09-18 01:12:43 +0900

    빈 등록시 중복 충돌 관련

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,8 +1,12 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
 
 /*
  * @Component 애노테이션이 붙은 클래스를 찾아서 자동으로 스프링빈으로 등록해준다.
@@ -12,12 +16,23 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
 		//이렇게하면 member만 컴포넌트 스캔 대상이된다. ("지정한곳"부터 하위패키지까지 쭉 찾는다)
-		basePackages = "hello.core.member",
+//		basePackages = "hello.core.member",
+		
+		//AutoAppConfig가 존재하는 hello.core 부터 시작된다
+//		basePackageClasses = AutoAppConfig.class,
 		//excludeFilters 모든 빈을 스캔하면서 제외할(필터) 명단
 		//Configuration도 자동스캔 대상이라 제외를 하지않으면? 이중으로 스캔하기때문에 충돌이 일어난다.
+		
+		//default 값은 여기 클래스부터 하위 패키지를 전부 스캔한다.
 		excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 		)
 public class AutoAppConfig {
+	
+	
+	@Bean(name = "memoryMemberRepository")
+	MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
 	
 	
 	
--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component	// "m"emoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository{
 
 	//원래는 동시성 문제 때문에 컨커런트 해쉬맵을 써야한다.
