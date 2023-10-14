6cfedf0a9a503083741a03ccdbafbff01b704ad8
kimmustard <kimustard44@gmail.com>
2023-10-15 02:53:16 +0900

    로그인 - 서블릿 필터

---
+++ login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,37 @@
+package hello.login;
+
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public FilterRegistrationBean<Filter> logFilter() {
+		
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new LogFilter());
+		filterRegistrationBean.setOrder(1);	//체인의 순서
+		filterRegistrationBean.addUrlPatterns("/*"); //url의 패턴
+		
+		return filterRegistrationBean;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<Filter> loginCheckFilter() {
+		
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new LoginCheckFilter());
+		filterRegistrationBean.setOrder(2);	//체인의 순서
+		filterRegistrationBean.addUrlPatterns("/*"); //url의 패턴
+		
+		return filterRegistrationBean;
+	}
+}
