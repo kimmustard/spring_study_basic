311fccf3b0a2a7dd2e0dae409ae62abd001d88b9
kimmustard <kimustard44@gmail.com>
2023-11-10 04:50:07 +0900

    인터페이스 기반 프록시 적용과 구체 클래스 프록시 적용

--- proxy/src/test/java/hello/proxy/pureproxy/code/CacheProxy.java
+++ proxy/src/test/java/hello/proxy/pureproxy/code/CacheProxy.java
@@ -23,6 +23,4 @@ public class CacheProxy implements Subject{
 		return cacheValue;
 	}
 	
-	
-
 }
--- proxy/src/test/java/hello/proxy/pureproxy/code/Subject.java
+++ proxy/src/test/java/hello/proxy/pureproxy/code/Subject.java
@@ -1,7 +1,5 @@
 package hello.proxy.pureproxy.code;
 
 public interface Subject {
-	
 	String operation();
-	
 }
